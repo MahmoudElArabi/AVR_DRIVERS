f3bf3a4febc8cc545204e5dcf142f3ce061f4c9c
Mahmoud Elaraby <mahmoudellarabi21@gmail.com>
2023-10-31 13:37:02 +0200

    I2C /TWI Module Implementation

---
+++ MCAL/TWI/TWI_Interface.h
@@ -0,0 +1,45 @@
+/*
+ * I2C_Interface.h
+ *
+ *  Created on: Oct 31, 2023
+ *      Author: Mahmoud El Arabi
+ */
+
+#ifndef MCAL_TWI_TWI_INTERFACE_H_
+#define MCAL_TWI_TWI_INTERFACE_H_
+
+
+/* ----------------- Section : includes -----------------*/
+#include "../PORT/PORT_Interface.h"
+#include "TWI_Registers.h"
+
+/* ----------------- Section : Macro Declarations -----------------*/
+#define STATUS_VALUE					(_TWSR & 0xF8)
+
+#define Start_Condition_ACK				0x08
+#define RepeatedStart_Condition_ACK		0x10
+
+#define SlaveAddress_Write_ACK			0x18
+#define SlaveAddress_Read_ACK			0x40
+
+#define Master_Data_Transmit_ACK		0x28
+#define Master_Data_Receive_ACK			0x50
+
+/* ----------------- Section : Data types Declarations -----------------*/
+
+
+/* ----------------- Section : Software Interfaces Declaration -----------------*/
+
+// Master APIs
+void TWI_voidInitMaster(u8 copy_u8MsterAddress);
+void TWI_voidSendStartCondition(void);
+void TWI_voidSendStopCondition(void);
+void TWI_voidSendReapeatedStartCondition(void);
+void TWI_voidSendSlaveAdd_WriteRequest(u8 copy_u8SlaveAddress);
+void TWI_voidSendSlaveAdd_ReadRequest(u8 copy_u8SlaveAddress);
+void TWI_voidWriteMasterDataByte(u8 copy_u8Data);
+void TWI_voidReadMasterDataByte(u8 *copy_u8PtrData);
+
+//Slave APIs
+
+#endif /* MCAL_TWI_TWI_INTERFACE_H_ */
--- main.c
+++ main.c
@@ -7,27 +7,13 @@
 
 #include "main_h.h"
 
-void ACT_SPI(u8 Copy_u8Recrived)
-{
-	if(Copy_u8Recrived == 70)
-	{
-		Dio_FlipChannel(PA_4);
-	}
-}
 
 int main() {
 	Port_Init(pins);
 	GI_voidEnable();
 
-	SPI_voidInit();
-
 	while (1)
 	{
-		Dio_WriteChannel(PB_4, 1);
-		_delay_ms(1);
-		Dio_WriteChannel(PB_4, 0);
-		SPI_voidTransmit_Async(2, ACT_SPI);
-		_delay_ms(200);
 	}
 	return 0;
 }
